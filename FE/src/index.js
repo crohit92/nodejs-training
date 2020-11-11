1e692f0634d3aba43b5044414d690b61c822e28a
Rohit Chopra <crohit92@gmail.com>
2020-11-11 14:07:47 +0530

    WIP Implement update of todos

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,23 +1,45 @@
+var id;
 function saveTodo() {
     const todo = {
         isComplete: false,
         title: document.getElementById('title').value,
         description: document.getElementById('description').value,
     };
-    fetch('http://localhost:3300/todos', {
-        method: 'POST',
-        body: JSON.stringify(todo),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            // listTodos();
-            appendTodo(res, document.getElementById('todos'));
-            console.log(res);
+    if (id === undefined) {
+        fetch('http://localhost:3300/todos', {
+            method: 'POST',
+            body: JSON.stringify(todo),
+            headers: {
+                'Content-Type': 'application/json',
+            },
         })
-        .catch((err) => console.error(err));
+            .then((res) => res.json())
+            .then((res) => {
+                // listTodos();
+                appendTodo(res, document.getElementById('todos'));
+                console.log(res);
+            })
+            .catch((err) => console.error(err));
+    } else {
+        fetch(`http://localhost:3300/todos/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify(todo),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                // listTodos();
+                document.getElementById(id).querySelector('.title').innerHTML =
+                    res.title;
+                id = undefined;
+                console.log(res);
+            })
+            .catch((err) => console.error(err));
+    }
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
 }
 
 function listTodos() {
@@ -38,16 +60,23 @@ window.onload = function () {
     listTodos();
 };
 function appendTodo(todo, todosContainer) {
+    console.log(todo);
     const li = document.createElement('li');
     li.setAttribute(
         'class',
         'my-2 p-3 border rounded d-flex flex-wrap justify-content-between'
     );
+    li.setAttribute('id', todo.id);
     li.innerHTML = `
                <div class="title">${todo.title}</div>
                <div class="status">
                   <input type="checkbox">
                </div>`;
+    li.onclick = function (ev) {
+        document.getElementById('title').value = todo.title;
+        document.getElementById('description').value = todo.description;
+        id = todo.id;
+    };
     li.querySelector("input[type='checkbox']").checked = todo.isComplete;
     todosContainer.appendChild(li);
 }
